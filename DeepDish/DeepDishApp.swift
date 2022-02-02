3525d88ff7ed5f2aacc81cb5383d703825e0a407
Anthony Iamurri <anthony.iamurri@gmail.com>
2022-02-02 13:49:05 -0500

    Initial Commit

---
+++ DeepDish/DeepDishApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepDishApp.swift
+//  DeepDish
+//
+//  Created by Anthony Iamurri on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct DeepDishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
