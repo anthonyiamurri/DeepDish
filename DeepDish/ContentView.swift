df9f45979e3a1d8707829f5efed0c84cd5c255d6
Anthony Iamurri <anthony.iamurri@gmail.com>
2022-02-03 14:11:56 -0500

    Changed text color to green

--- DeepDish/ContentView.swift
+++ DeepDish/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("It's the Dapplers!  Bom-bom-bom-bom-bom!")
+            .foregroundColor(Color.green)
             .padding()
     }
 }
