eb2648c7d7349ef6f1b86057f3ffd212560ccf74
Anthony Iamurri <anthony.iamurri@gmail.com>
2022-02-11 14:29:30 -0500

    added LazyVGrid to bottom section

--- DeepDish/HomeScreenGroup/CategorieStruct.swift
+++ DeepDish/HomeScreenGroup/CategorieStruct.swift
@@ -7,26 +7,32 @@
 
 import Foundation
 
-struct Row: Identifiable{
-    let id = UUID()
-    var categories: [Categorie]
-}
-struct Categorie: Identifiable{
+//struct Row: Identifiable{
+//    let id = UUID()
+//    var categories: [Categorie]
+//}
+struct Categorie: Identifiable {
+    // instance variables (fields)
     let id = UUID()
     var Text : String
     var Image: String
     
+    //class variables (defined at the entire structure)
+    //singleton (static variables)
     static var firstCategorie: Categorie = Categorie(Text: "first", Image: "pasta")
     static var secondCategorie: Categorie = Categorie(Text: "second", Image: "pasta")
+    static var thirdCategorie: Categorie = Categorie(Text: "third", Image: "pasta")
+    
+    static let TestData: [Categorie] = [firstCategorie,secondCategorie,thirdCategorie]
 
-    static let categoriesRow1 = Row(categories: [firstCategorie,secondCategorie])
-    static let categoriesRow2 = Row(categories: [firstCategorie,secondCategorie])
-    static let categoriesRow3 = Row(categories: [firstCategorie,secondCategorie])
-    static let categoriesRow4 = Row(categories: [firstCategorie,secondCategorie])
-    static let categoriesRow5 = Row(categories: [firstCategorie,secondCategorie])
-    static let categoriesRow6 = Row(categories: [firstCategorie,secondCategorie])
+//    static let categoriesRow1 = Row(categories: [firstCategorie,secondCategorie])
+//    static let categoriesRow2 = Row(categories: [firstCategorie,secondCategorie])
+//    static let categoriesRow3 = Row(categories: [firstCategorie,secondCategorie])
+//    static let categoriesRow4 = Row(categories: [firstCategorie,secondCategorie])
+//    static let categoriesRow5 = Row(categories: [firstCategorie,secondCategorie])
+//    static let categoriesRow6 = Row(categories: [firstCategorie,secondCategorie])
 
-    static let TestData: [Row] = [categoriesRow1,categoriesRow2,categoriesRow3,categoriesRow4,categoriesRow5,categoriesRow6]
+//    static let TestData: [Row] = [categoriesRow1,categoriesRow2,categoriesRow3,categoriesRow4,categoriesRow5,categoriesRow6]
 }
 
 
--- DeepDish/HomeScreenGroup/HomeScreenButtomSectionView.swift
+++ DeepDish/HomeScreenGroup/HomeScreenButtomSectionView.swift
@@ -18,43 +18,38 @@ struct HomeScreenButtomSectionView: View {
                 .frame(width: 5, height: 5)
                 .foregroundColor(.white)
             ScrollView{
-                VStack {
-                    ForEach(Categorie.TestData){ index in
-                        HStack{
-                            ForEach(index.categories){ index in
-                                Spacer()
-                                Image(index.Image)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 150, height: 150)
-                                    .clipShape(RoundedRectangle(cornerRadius: 30))
-                                    .shadow(color: Color.black.opacity(0.2), radius:1, x:0,y:5)
-                                    .overlay(ZStack {
-                                        Text(index.Text)
-                                            .font(.callout)
-                                            .bold()
-                                            .padding(6)
-                                            .foregroundColor(.white)
-                                    }.background(Color.red)
-                                                .cornerRadius(20.0)
-                                                .padding(6.0)
-                                             , alignment: .bottomTrailing)
-                                    
-                            }
-                            Spacer()
-                        }
+                let gridItems: [GridItem] = Array(repeating: .init(.adaptive(minimum: 200)), count: 2)
+                LazyVGrid(columns: gridItems) {
+                    ForEach(Categorie.TestData) { categorie in
+                        Image(categorie.Image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 150, height: 150)
+                            .clipShape(RoundedRectangle(cornerRadius: 30))
+                            .shadow(color: Color.black.opacity(0.2), radius:1, x:0,y:5)
+                            .overlay(ZStack {
+                                Text(categorie.Text)
+                                    .font(.callout)
+                                    .bold()
+                                    .padding(6)
+                                    .foregroundColor(.white)
+                            }.background(Color.red)
+                                        .cornerRadius(20.0)
+                                        .padding(6.0)
+                                     , alignment: .bottomTrailing)
                         
                     }
                 }
+                
             }
         }
-        
     }
 }
+
 
 struct CategoriesView_Previews: PreviewProvider {
     static var previews: some View {
         HomeScreenButtomSectionView()
-            
+        
     }
 }
