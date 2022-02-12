34f3af64e7d5205fa7f2a5ef98df4b3f7cc0e046
mofe123 <mofe12>
2022-02-12 03:12:36 -0500

    added categorie swift to fix error

---
+++ DeepDish/HomeScreenGroup/CategorieStruct.swift
@@ -0,0 +1,20 @@
+//
+//  CategorieStruct.swift
+//  DeepDish
+//
+//  Created by Eyimofe Oladipo on 2/12/22.
+//
+
+import SwiftUI
+
+struct CategorieStruct: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategorieStruct_Previews: PreviewProvider {
+    static var previews: some View {
+        CategorieStruct()
+    }
+}
